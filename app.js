8e7fb7131a32c2cabfe7406ad379d46c91ebe0f0
CoolRy <that007guy@hotmail.com>
2022-05-29 14:05:11 -0400

    complete

--- app.js
+++ app.js
@@ -3,16 +3,38 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
-  previousGuess: [],
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
-}
-getGuess: function(){
-  let guess
-  while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum)
-  guess = parseInt(prompt(`Enter a number between ${this.smallestNum} and ${this.smallestNum}`))
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum 
+      console.log(this.secretNum)
+      do {
+        this.prevGuesses.push(this.getGuess())
+        console.log(this.prevGuesses)
+        this.render()
+      } while (this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
   
+  },
+  getGuess: function(){
+    let guess
+    do {
+       guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+    } while (guess < this.smallestNum || guess > this.biggestNum)
+    return guess
+  },
+  render: function() {
+    if (this.prevGuesses[this.prevGuesses.length -1] === this.secretNum){
+      alert(`Congrats you guessed the ${this.secretNum}`)
+    } else if (this.prevGuesses[this.prevGuesses.length -1] > this.secretNum){
+      alert(`Your guess is to High guess again`)
+    } else if (this.prevGuesses[this.prevGuesses.length -1] < this.secretNum){
+      alert(`Your guess is to Low guess again`)
+    }
+
+
+
+
+  }
+
 }
-return guess
+game.play()
